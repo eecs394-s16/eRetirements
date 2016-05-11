808faf29c9a390298585333ae1574b1cc6d6bc0e
DMlibraXAQ <aiqixie2016@u.northwestern.edu>
2016-05-10 21:20:59 -0500

    finished skip button, finished progress bar, need to add filled percentage, need to record user results

--- questionScript.js
+++ questionScript.js
@@ -2,51 +2,71 @@ var categories = {
 	"location": [
 		{
 			"question": "My perfect weather...",
-			"answers": ["is warm year-round, with as little rain as possible","is warm year-round, but I don't mind some showers","includes all 4 seasons, with maybe SOME snow","definitely has some snow"]
+			"answers": ["is warm year-round, with as little rain as possible","is warm year-round, but I don't mind some showers","includes all 4 seasons, with maybe SOME snow","definitely has some snow"],
+			"results":""
 		},
 		{
 			"question": "Close your eyes and picture your ideal day. You are...",
-			"answers": ["spending some time on the beach","hitting the slopes","going for a hike in the woods","shopping/relaxing at home with a book"]
+			"answers": ["spending some time on the beach","hitting the slopes","going for a hike in the woods","shopping/relaxing at home with a book"],
+			"results":""
+		},
+		{
+			"question": "My ideal location is...",
+			"answers": ["A Quaint Town (Think Little House on the Prairie)","A Big Town (Think Myrtle Beach)","A Small City (Think Sarasota)","A Big City (Think Dallas)"],
+			"results":""
 		}
 	],
 	"activities": [
 		{
 			"question": "Golf...",
-			"answers": ["is the love of my life (besides my spouse of course)","is fun every now and then","doesn't interest me"]
+			"answers": ["is the love of my life (besides my spouse of course)","is fun every now and then","doesn't interest me"],
+			"results":""
 		},
 		{
 			"question": "When it comes to historical sites and museums,",
-			"answers": ["I love them both","the history is great, but I can do without the museums","an occasional day at the museum is always enriching","I haven't been to one since my 5th grade field trip"]
+			"answers": ["I love them both","the history is great, but I can do without the museums","an occasional day at the museum is always enriching","I haven't been to one since my 5th grade field trip"],
+			"results":""
+		},
+		{
+			"question": "When it comes to food, I like to,",
+			"answers": ["Try as many restaurants as possible, I'm a bit of a foodie","Find a few favorite restaurants and stick to them","Forget restaurants-I'm a takeout/fast food person","I don't go out to eat often"],
+			"results":""
 		}
 	],
 	"community": [
 		{
 			"question": "The culture of a college town...",
-			"answers": ["sounds appealing","doesn't really interest me","I don't care either way"]
+			"answers": ["sounds appealing","doesn't really interest me","I don't care either way"],
+			"results":""
 		},
 		{
 			"question": "I expect to travel...",
-			"answers": ["all over the world","a lot, but mostly domestic","a little, but expect a lot of visitors","none - why would I leave my perfect new home?"]
+			"answers": ["all over the world","a lot, but mostly domestic","a little, but expect a lot of visitors","none - why would I leave my perfect new home?"],
+			"results":""
 		}
 	],
 	"costs": [
 		{
 			"question": "I plan on buying a home...",
-			"answers": ["cheap - my money is best spent elsewhere","average priced","I don't mind splurging - it's my retirement dream home for crying out loud!","I plan on renting, so this doesn't apply to me."]
+			"answers": ["cheap - my money is best spent elsewhere","average priced","I don't mind splurging - it's my retirement dream home for crying out loud!","I plan on renting, so this doesn't apply to me."],
+			"results":""
 		},
 		{
 			"question": "My living costs during retirement...",
-			"answers": ["are not a concern","are somewhat important","are something I need to keep a close eye on","are a top priority - I'm on a tight budget"]
+			"answers": ["are not a concern","are somewhat important","are something I need to keep a close eye on","are a top priority - I'm on a tight budget"],
+			"results":""
 		}
 	],
 	"health": [
 		{
 			"question": "The quality of the clinical care...",
-			"answers": ["needs to be the best","should be better than the average","average quality of care is just fine","doesn't matter to me, I don't go to the doctor often"]
+			"answers": ["needs to be the best","should be better than the average","average quality of care is just fine","doesn't matter to me, I don't go to the doctor often"],
+			"results":""
 		},
 		{
 			"question": "The cost of assisted living in the area should be...",
-			"answers": ["a good value","about average","irrelevant - I'll pay whatever it takes for the best","haven't thought about that just yet"]
+			"answers": ["a good value","about average","irrelevant - I'll pay whatever it takes for the best","haven't thought about that just yet"],
+			"results":""
 		}
 	]
 }
