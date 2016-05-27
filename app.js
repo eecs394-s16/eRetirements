d2e6f6c64c5d876ba8104b66c3750ec58be235fe
Eli <elicohen2018@u.northwestern.edu>
2016-05-26 20:48:21 -0500

    fixed server errors, switched jsonp to json

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express'),
   app = express(),
-  OAuth = require('oauth');
+  path = require("path");
 
   var port = process.env.PORT || 8000;
+
+  app.use(express.static(__dirname));
 
   app.get('/',function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'));
   });
 
   app.listen(port);
+  console.log("listening on port " + port)
