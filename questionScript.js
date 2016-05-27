6013058dad6d5b82a2264bdaa568ae6e1ada4bb4
babaozhou <leizhou2015@u.northwestern.edu>
2016-05-26 20:36:26 -0500

    get value

--- questionScript.js
+++ questionScript.js
@@ -15,7 +15,7 @@ var ratings = {
 };
 var categories = {
 	"location": [
-		{
+		{	"name": "weatherType",
 			"question": "My perfect weather...",
 			"answers": ["is warm year-round, with as little rain as possible","is warm year-round, but I don't mind some showers","includes all 4 seasons, with maybe SOME snow","definitely has some snow"],
 			"values": ["warm", "warm-showers", "all-seasons", "some-snow"],
@@ -23,6 +23,7 @@ var categories = {
 			
 		},
 		{
+			"name": "vacationDay",
 			"question": "Close your eyes and picture your ideal day. You are...",
 			"answers": ["spending some time on the beach","hitting the slopes","going for a hike in the woods","shopping/relaxing at home with a book"],
 			"values": ["beach", "mountain", "forest", "home"],
@@ -30,6 +31,7 @@ var categories = {
 			
 		},
 		{
+			"name": "communitySize",
 			"question": "My ideal location is...",
 			"answers": ["A Quaint Town (Think Little House on the Prairie)","A Big Town (Think Myrtle Beach)","A Small City (Think Sarasota)","A Big City (Think Dallas)"],
 			"values": ["quaint-town", "big-town", "small-city", "big-city"],
@@ -39,6 +41,7 @@ var categories = {
 	],
 	"activities": [
 		{
+			"name": "golf",
 			"question": "Golf...",
 			"answers": ["is the love of my life (besides my spouse of course)","is fun every now and then","doesn't interest me"],
 			"values": ["important", "occasionally", "no", ""],
@@ -46,6 +49,7 @@ var categories = {
 			
 		},
 		{
+			"name": "museumsAndHistoricalSites",
 			"question": "When it comes to historical sites and museums,",
 			"answers": ["I love them both","the history is great, but I can do without the museums","an occasional day at the museum is always enriching","I haven't been to one since my 5th grade field trip"],
 			"values": ["both", "history", "museums", "none"],
@@ -53,6 +57,7 @@ var categories = {
 			
 		},
 		{
+			"name": "food",
 			"question": "When it comes to food, I like to,",
 			"answers": ["Try as many restaurants as possible, I'm a bit of a foodie","Find a few favorite restaurants and stick to them","Forget restaurants-I'm a takeout/fast food person","I don't go out to eat often"],
 			"values": ["foodie", "routine", "fast-food", "home-cook"],
@@ -60,6 +65,7 @@ var categories = {
 			
 		},
 		{
+			"name": "work",
 			"question": "In my new location I will,",
 			"answers": ["Work full time - have to pay the bills!","Work part time - got to stay busy","Forget work - I'm ready to relax"],
 			"values": ["full-time", "part-time", "none", ""],
@@ -69,6 +75,7 @@ var categories = {
 	],
 	"community": [
 		{
+			"name": "getAround",
 			"question": "I prefer to get to work or around the community by..",
 			"answers": ["Walking","Car","Public Transit","Taxi/Bike/Motorcycle/Other"],
 			"values": ["walking", "car", "public-transit", "other"],
@@ -76,6 +83,7 @@ var categories = {
 			
 		},
 		{
+			"name": "collegeTowns",
 			"question": "The culture of a college town...",
 			"answers": ["sounds appealing","doesn't really interest me","I don't care either way"],
 			"values": ["yes", "no", "any", ""],
@@ -83,6 +91,7 @@ var categories = {
 			
 		},
 		{
+			"name": "idealCommunity",
 			"question": "My ideal community (select all that apply)..",
 			"answers": ["Has a lot of young people","Is very volunteer focused","Is higher income","Is physically active"],
 			"values": ["young", "volunteering", "wealthy", "active"],
@@ -90,6 +99,7 @@ var categories = {
 			
 		},
 		{
+			"name": "averageAge",
 			"question": "I prefer to be with people in the following age range..",
 			"answers": ["Early retirees","Retirement aged","Late retirees","Mix of families and retirees"],
 			"values": ["55-64", "65-74", "75+", "any"],
@@ -97,6 +107,7 @@ var categories = {
 			
 		},
 		{
+			"name": "travel",
 			"question": "I expect to travel...",
 			"answers": ["all over the world","a lot, but mostly domestic","a little, but expect a lot of visitors","none - why would I leave my perfect new home?"],
 			"values": ["world", "domestic", "visitors", "no"],
@@ -106,6 +117,7 @@ var categories = {
 	],
 	"costs": [
 		{
+			"name": "buyingHome",
 			"question": "I plan on buying a home...",
 			"answers": ["cheap - my money is best spent elsewhere","average priced","I don't mind splurging - it's my retirement dream home for crying out loud!","I plan on renting, so this doesn't apply to me."],
 			"values": ["cheap", "average", "expensive", "rent"],
@@ -113,6 +125,7 @@ var categories = {
 			
 		},
 		{
+			"name": "taxes",
 			"question": "When it comes to taxes...",
 			"answers": ["I would sacrifice quality of life for the lowest taxes possible","Taxes are inevitable, but I would like to keep them low","I can deal with an average amount of taxes","Completely worth it for the right location"],
 			"values": ["lowest", "below-average", "average", "any"],
@@ -120,6 +133,7 @@ var categories = {
 			
 		},
 		{
+			"name": "livingCosts",
 			"question": "My living costs during retirement...",
 			"answers": ["are not a concern","are somewhat important","are something I need to keep a close eye on","are a top priority - I'm on a tight budget"],
 			"values": ["any", "important", "very-important", "budget"],
@@ -129,6 +143,7 @@ var categories = {
 	],
 	"health": [
 		{
+			"name": "clinicalQuality",
 			"question": "The quality of the clinical care...",
 			"answers": ["needs to be the best","should be better than the average","average quality of care is just fine","doesn't matter to me, I don't go to the doctor often"],
 			"values": ["best", "above-average", "average", "any"],
@@ -136,6 +151,7 @@ var categories = {
 			
 		},
 		{
+			"name": "assistedLivingCosts",
 			"question": "The cost of assisted living in the area should be...",
 			"answers": ["a good value","about average","irrelevant - I'll pay whatever it takes for the best","haven't thought about that just yet"],
 			"values": ["low", "above-average", "top", "undecided"],
@@ -143,6 +159,7 @@ var categories = {
 			
 		},
 		{
+			"name": "nursingHomes",
 			"question": "The local nursing homes...",
 			"answers": ["Need to be the best in the business","Need to be strong, but not the best","Should be a good value","Nursing Home? Not even on my radar yet"],
 			"values": ["best", "strong", "average", "none"],
@@ -150,4 +167,40 @@ var categories = {
 			
 		}
 	]
+}
+var dataForApi = {
+    "region": ["south", "north", "west", "east"],
+    "communitySize": "quaint-town",
+    "vacationDay": "beach",
+    "weatherType": "warm",
+    "idealCommunity": ["young"],
+    "livingCosts": "any",
+    "golf": "important",
+    "averageAge": "55-64",
+    "getAround": "running",
+    "clinicalQuality": "best",
+    "food": "foodie",
+    "assistedLivingCosts": "low",
+    "collegeTowns": "yes",
+    "work": "full-time",
+    "museumsAndHistoricalSites": "both",
+    "nursingHomes": "best",
+    "buyingHome": "cheap",
+    "travel": "world",
+    "taxes": "lowest",
+    "weather": -1,
+    "location": -1,
+    //////
+    "activities": -1,
+    "workPotential": -1,
+    //////
+    "community": -1,
+    "demographics": -1,
+    //////
+    "healthcare": -1,
+    /////
+    "costs": -1,
+    
+    "restrictRegion": false
+
 }
