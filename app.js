22e1d8ff3a4ef15af4d95c5c87a5855d34a70bea
Eli <elicohen2018@u.northwestern.edu>
2016-05-26 21:12:56 -0500

    pasted sample app

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express'),
-  app = express(),
-  path = require("path");
+var express = require('express');
+var app = express();
 
-  var port = process.env.PORT || 8000;
+app.set('port', (process.env.PORT || 5000));
 
-  app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
-  app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname + '/index.html'));
-  });
+app.get('/', function(request, response) {
+  res.send('index.html');
+});
 
-  app.listen(port);
-  console.log("listening on port " + port)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
